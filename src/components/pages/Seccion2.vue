f07b2d6aae8c602e9a9631b4fe8f9d25eaeaf189
MarioPuc <74841477+MarioPuc@users.noreply.github.com>
2023-01-10 11:57:05 -0600

    added new return button

--- src/components/pages/Seccion2.vue
+++ src/components/pages/Seccion2.vue
@@ -1,95 +1,113 @@
 <template>
-  <div 
-    class="text-center theme-wedding"
+  <div class="text-center theme-wedding"
     :style="`background-image: url('https://workspacedigiart.com/img/fondos/${this.datosApp.evento.fondo}');`"
-    v-if="visibles.bienvenida == false && visibles.confirmacionAsistencia == true"
-  >
+    v-if="visibles.bienvenida == false && visibles.confirmacionAsistencia == true">
     <div class="container full-height">
-        <v-container fill-height fluid>
-          <v-row align="center"
-              justify="center">
+      <v-container fill-height fluid>
+        <v-row align="center" justify="center">
           <div v-if="datosPrincipales">
             <div style="display: none;">
-            <p
-              class="pb-2y" 
-              :style="`font-size:${this.datosApp.evento.tamanoFuenteTitulo}px; font-family:${this.datosApp.evento.fuenteTitulo}; color:${this.datosApp.evento.colorFuenteTitulo};`"> 
+              <p class="pb-2y"
+                :style="`font-size:${this.datosApp.evento.tamanoFuenteTitulo}px; font-family:${this.datosApp.evento.fuenteTitulo}; color:${this.datosApp.evento.colorFuenteTitulo};`">
                 {{ this.datosApp.evento.nombreOwner }}
-            </p>
+              </p>
 
-            <p>Te invitamos a celebrar con nosotros</p>
+              <p>Te invitamos a celebrar con nosotros</p>
 
-            <p :style="`font-size: 35px; font-family:${this.datosApp.evento.fuenteTitulo}; `" >Nuestra Boda</p>
+              <p :style="`font-size: 35px; font-family:${this.datosApp.evento.fuenteTitulo}; `">Nuestra Boda</p>
 
-            <p>{{ this.datosApp.evento.fechaEvento }}</p>
+              <p>{{ this.datosApp.evento.fechaEvento }}</p>
 
-            <p>RECEPCIÓN</p>
+              <p>RECEPCIÓN</p>
 
-            <a :href="coordinatesGrades" target="_blank">
-              <v-btn color="#E6C98A" class="white--text my-2" tile elevation="0">Ver evento en google maps</v-btn>
-            </a>
+              <a :href="coordinatesGrades" target="_blank">
+                <v-btn color="#E6C98A" class="white--text my-2" tile elevation="0">Ver evento en google maps</v-btn>
+              </a>
 
-            <p>{{ this.datosApp.evento.horaEvento }} hrs</p>
+              <p>{{ this.datosApp.evento.horaEvento }} hrs</p>
 
-            <p>Código de vestimenta: </p>
+              <p>Código de vestimenta: </p>
 
-            <p>{{ this.datosApp.evento.codigoVestimenta }}</p>
+              <p>{{ this.datosApp.evento.codigoVestimenta }}</p>
 
-            <a :href="this.datosApp.evento.linkMesas" target="_blank">
-              <v-btn color="#E6C98A" class="white--text" tile elevation="0">Ver mesa de regalos</v-btn>
-            </a>
-            <div>
-              <v-btn color="#E6C98A" class="white--text my-5" tile elevation="0" v-on:click="() => { this.datosPrincipales = !this.datosPrincipales }">
-                <v-icon>mdi-arrow-right-bold</v-icon>
-            </v-btn>
+              <a :href="this.datosApp.evento.linkMesas" target="_blank">
+                <v-btn color="#E6C98A" class="white--text" tile elevation="0">Ver mesa de regalos</v-btn>
+              </a>
+              <div>
+                <v-btn color="#E6C98A" class="white--text my-5" tile elevation="0"
+                  v-on:click="() => { this.datosPrincipales = !this.datosPrincipales }">
+                  <v-icon>mdi-arrow-right-bold</v-icon>
+                </v-btn>
+              </div>
             </div>
-          </div>
-
-          <div>
-            <div class="text-center font white--text " style="font-family: GaramondMedIt; font-size: 50px;">
-              SPARKLE &
-            </div>
-            <div class="text-center font white--text " style="font-family: GaramondMedIt; font-size: 60px; line-height: 30px;">
-              SHINE
-            </div>
-
-            <div class="text-center font white--text " style="font-family: SofiaProRegular; font-size: 24px; line-height: 40px;">IT'S BIRTHDAY TIME!</div>
-            
-            <div class="text-center font white--text " style="font-family: SofiaProRegular; font-size: 30px; line-height: 35px; letter-spacing: 2px;">PATTY'S</div>
-
-            <div class="text-center font white--text " style="font-family: Colatin; font-size: 100px; line-height: 80px;">30 <sup style="font-family: Colatin; font-size: 60px;">th</sup></div>
-
-            <div class="text-center font white--text " style="font-family: SofiaProRegular; font-size: 30px; line-height: 35px; letter-spacing: 5px;">PARTY</div>
-
-
-            <div class="text-center font white--text " style="font-family: GaramondMedIt; font-size: 50px; line-height: 65px; letter-spacing: 1px;">21/ENE/23</div>
-
-            <div class="text-center font white--text " style="font-family: SofiaProRegular; font-size: 20px; letter-spacing: 1px;">CALLE 25 N° 303 X 4 Y 6 <br> FRACC: MONTEBELLO</div>
 
             <div>
-              <v-btn color="#E6C98A" class="white--text my-5" tile elevation="0" v-on:click="() => { this.datosPrincipales = !this.datosPrincipales }">
-                <v-icon>mdi-arrow-right-bold</v-icon>
-            </v-btn>
+              <div class="text-center font white--text " style="font-family: GaramondMedIt; font-size: 50px;">
+                SPARKLE &
+              </div>
+              <div class="text-center font white--text "
+                style="font-family: GaramondMedIt; font-size: 60px; line-height: 30px;">
+                SHINE
+              </div>
+
+              <div class="text-center font white--text "
+                style="font-family: SofiaProRegular; font-size: 24px; line-height: 40px;">IT'S BIRTHDAY TIME!</div>
+
+              <div class="text-center font white--text "
+                style="font-family: SofiaProRegular; font-size: 30px; line-height: 35px; letter-spacing: 2px;">PATTY'S
+              </div>
+
+              <div class="text-center font white--text "
+                style="font-family: Colatin; font-size: 100px; line-height: 80px;">30 <sup
+                  style="font-family: Colatin; font-size: 60px;">th</sup></div>
+
+              <div class="text-center font white--text "
+                style="font-family: SofiaProRegular; font-size: 30px; line-height: 35px; letter-spacing: 5px;">PARTY
+              </div>
+
+
+              <div class="text-center font white--text "
+                style="font-family: GaramondMedIt; font-size: 50px; line-height: 65px; letter-spacing: 1px;">21/ENE/23
+              </div>
+
+              <div class="text-center font white--text "
+                style="font-family: SofiaProRegular; font-size: 20px; letter-spacing: 1px;">
+                CALLE 25 N° 303 X 4 Y 6 <br> FRACC: MONTEBELLO
+              </div>
+
+              <a :href="coordinatesGrades" target="_blank">
+                <v-btn color="#E6C98A" class="white--text my-5" tile elevation="0">
+                  <v-icon>mdi-map-marker</v-icon> Ver ubicación en Google Maps
+                </v-btn>
+              </a>
+
+              <div>
+                <v-btn color="#E6C98A" class="white--text my-5" tile elevation="0"
+                  v-on:click="() => { this.datosPrincipales = !this.datosPrincipales }">
+                  <v-icon>mdi-arrow-right-bold</v-icon>
+                </v-btn>
+              </div>
             </div>
-          </div>
 
           </div>
           <div class="app-container" v-if="!datosPrincipales">
-            <p
-              class="pb-4" 
-              :style="`font-size:${this.datosApp.evento.tamanoFuenteTitulo}px; font-family:${this.datosApp.evento.fuenteTitulo}; color:${this.datosApp.evento.colorFuenteTitulo}; text-transform: uppercase;`"> 
-                {{ this.datosApp.evento.nombreOwner }}
+            <p class="pb-4"
+              :style="`font-size:${this.datosApp.evento.tamanoFuenteTitulo}px; font-family:${this.datosApp.evento.fuenteTitulo}; color:${this.datosApp.evento.colorFuenteTitulo}; text-transform: uppercase;`">
+              {{ this.datosApp.evento.nombreOwner }}
             </p>
 
             <p
-            :style="`font-size:${this.datosApp.evento.tamanoFuenteCuerpo}px; font-family:${this.datosApp.evento.fuenteCuerpo}; color:${this.datosApp.evento.colorFuenteCuerpo}`"
-            >Te / Les ha invitado a su fiesta de cumpleaños, por favor confirme su asistencia. 
-             </p>
-                  
-            <v-btn color="#E6C98A" class="white--text mx-2" tile elevation="0" v-on:click="confirmarAsistencia('si')" >Asistiré</v-btn>
-            <v-btn color="#E6C98A" class="white--text mx-2" tile elevation="0" v-on:click="confirmarAsistencia('no')" >No Asistiré</v-btn>
-          
+              :style="`font-size:${this.datosApp.evento.tamanoFuenteCuerpo}px; font-family:${this.datosApp.evento.fuenteCuerpo}; color:${this.datosApp.evento.colorFuenteCuerpo}`">
+              Te / Les ha invitado a su fiesta de cumpleaños, por favor confirme su asistencia.
+            </p>
+
+            <v-btn color="#E6C98A" class="white--text mx-2" tile elevation="0"
+              v-on:click="confirmarAsistencia('si')">Asistiré</v-btn>
+            <v-btn color="#E6C98A" class="white--text mx-2" tile elevation="0" v-on:click="confirmarAsistencia('no')">No
+              Asistiré</v-btn>
+
             <p class="pt-4 subtext d-none">Tiene hasta el {{ deadline }} a las 23:00 hrs</p>
-            </div>
+          </div>
         </v-row>
       </v-container>
     </div>
@@ -101,38 +119,37 @@ import { mapActions, mapState } from 'vuex';
 
 import * as moment from 'moment';
 
-    export default {
-      name: 'Seccion2',
-  
-      components: {
-      },
-  
-      data: () => ({
-        codigoEvento : "",
-        coordenadasGrados: "",
-        datosPrincipales: true
-      }),
+export default {
+  name: 'Seccion2',
 
-      computed: {
-        ... mapState(['visibles', 'datosApp']),
-        deadline() {
-          moment.locale('es-mx');
-          return moment(this.datosApp.evento.fechaEvento).subtract(2, 'months').format('LL');
-        },
-        coordinatesGrades() {
-          const objCoordenadas = this.datosApp.evento.locacionEvento;
-          return `https://www.google.com.mx/maps/search/${objCoordenadas.lat}, ${objCoordenadas.lng}`;
-        }
-      },
-  
-      methods: {
-        ... mapActions(['confirmacionAsistencia']),
+  components: {
+  },
 
-        confirmarAsistencia(status){
-          this.confirmacionAsistencia(status);
-        }
-      }
+  data: () => ({
+    codigoEvento: "",
+    coordenadasGrados: "",
+    datosPrincipales: true
+  }),
+
+  computed: {
+    ...mapState(['visibles', 'datosApp']),
+    deadline() {
+      moment.locale('es-mx');
+      return moment(this.datosApp.evento.fechaEvento).subtract(2, 'months').format('LL');
+    },
+    coordinatesGrades() {
+      const objCoordenadas = this.datosApp.evento.locacionEvento;
+      return `https://www.google.com.mx/maps/search/${objCoordenadas.lat}, ${objCoordenadas.lng}`;
     }
+  },
 
-  </script>
-  
+  methods: {
+    ...mapActions(['confirmacionAsistencia']),
+
+    confirmarAsistencia(status) {
+      this.confirmacionAsistencia(status);
+    }
+  }
+}
+
+</script>
--- src/components/pages/Seccion3.vue
+++ src/components/pages/Seccion3.vue
@@ -1,91 +1,86 @@
 <template>
-  <div 
-    class="text-center theme-wedding"
+  <div class="text-center theme-wedding"
     :style="`background-image: url('https://workspacedigiart.com/img/fondos/${this.datosApp.evento.fondo}');`"
-    v-if="(visibles.bienvenida == false && visibles.confirmacionAsistencia == false)"
-  >
+    v-if="(visibles.bienvenida == false && visibles.confirmacionAsistencia == false)">
     <div class="container full-height">
-        <v-container fill-height fluid>
-          <v-row align="center"
-              justify="center">
-    <div class="app-container white--text" >
-      <div v-if="visibleAsistentes">
-        <div v-if="visibles.asistencia == 'si'"> 
-            <h1 :style="`font-size:${this.datosApp.evento.tamanoFuenteTitulo}px; font-family:${this.datosApp.evento.fuenteTitulo}; color:${this.datosApp.evento.colorFuenteTitulo}`">¡GRACIAS POR SU <br> CONFIRMACIÓN!</h1>
+      <v-container fill-height fluid>
+        <v-row align="center" justify="center">
+          <div class="app-container white--text">
+            <div v-if="visibleAsistentes">
+              <div v-if="visibles.asistencia == 'si'">
+                <h1
+                  :style="`font-size:${this.datosApp.evento.tamanoFuenteTitulo}px; font-family:${this.datosApp.evento.fuenteTitulo}; color:${this.datosApp.evento.colorFuenteTitulo}`">
+                  ¡GRACIAS POR SU <br> CONFIRMACIÓN!</h1>
 
-            <div>
+                <div>
 
-              <p :style="`font-size:${this.datosApp.evento.tamanoFuenteCuerpo}px; font-family:${this.datosApp.evento.fuenteCuerpo}; color:${this.datosApp.evento.colorFuenteCuerpo}`">¿Podría decirnos cuantos <br /> asistentes serán?</p>
+                  <p
+                    :style="`font-size:${this.datosApp.evento.tamanoFuenteCuerpo}px; font-family:${this.datosApp.evento.fuenteCuerpo}; color:${this.datosApp.evento.colorFuenteCuerpo}`">
+                    ¿Podría decirnos cuantos <br /> asistentes serán?</p>
 
-              <v-form>
-                <v-select
-                    v-model="totalAsistentes"
-                    label="Número de asistentes"
-                    :items="arrayAsistentes"
-                    color="#000"
-                ></v-select>
+                  <v-form>
+                    <v-select v-model="totalAsistentes" label="Número de asistentes" :items="arrayAsistentes"
+                      color="#000"></v-select>
 
-                <v-btn
-                  color="#E6C98A"
-                  class="white--text"
-                  tile
-                  elevation="0"
-                  v-on:click="setNumeroAsistentes()"
-                >Añadir número de invitados</v-btn>
-              </v-form>
+                    <v-btn color="#E6C98A" class="white--text" tile elevation="0"
+                      v-on:click="setNumeroAsistentes()">Añadir número de invitados</v-btn>
+                  </v-form>
+                </div>
+
+              </div>
+
+              <div v-if="visibles.asistencia == 'no'">
+                <h1
+                  :style="`font-size:${this.datosApp.evento.tamanoFuenteTitulo}px; font-family:${this.datosApp.evento.fuenteTitulo}; color:${this.datosApp.evento.colorFuenteTitulo}`">
+                  LAMENTAMOS QUE NO PUEDAS ASISTIR</h1>
+
+                <p
+                  :style="`font-size:${this.datosApp.evento.tamanoFuenteCuerpo}px; font-family:${this.datosApp.evento.fuenteCuerpo}; color:${this.datosApp.evento.colorFuenteCuerpo}`">
+                  Si cambias de opinión puedes cambiar tu selección
+                </p>
+
+                <p class="pt-4 subtext d-none"> {{ deadline }} a las 23:00 hrs</p>
+
+                <span>Regresar a la página principal</span><br>
+
+                <a href="/">
+                  <v-btn color="#E6C98A" class="white--text my-5" tile elevation="0">
+                    <v-icon>mdi-arrow-u-left-bottom</v-icon>
+                  </v-btn>
+                </a>
+              </div>
             </div>
-            
-        </div>
+            <div v-else>
 
-        <div v-if="visibles.asistencia == 'no'">
-          <h1 :style="`font-size:${this.datosApp.evento.tamanoFuenteTitulo}px; font-family:${this.datosApp.evento.fuenteTitulo}; color:${this.datosApp.evento.colorFuenteTitulo}`">LAMENTAMOS QUE NO PUEDAS ASISTIR</h1>
+              <h3>Nombre: </h3>
 
-          <p :style="`font-size:${this.datosApp.evento.tamanoFuenteCuerpo}px; font-family:${this.datosApp.evento.fuenteCuerpo}; color:${this.datosApp.evento.colorFuenteCuerpo}`">
-            Si cambias de opinión puedes cambiar tu selección
-          </p>
+              <p>{{ this.datosApp.invitado.nombreInvitado }}</p>
 
-          <p class="pt-4 subtext d-none"> {{ deadline }} a las 23:00 hrs</p>
+              <h3>Número de acompañantes: </h3>
 
-          <span>Regresar a la página principal</span><br>
+              <p>{{ this.datosApp.invitado.totalAsistentes }}</p>
 
-          <a href="/">
-          <v-btn color="#E6C98A" class="white--text my-5" tile elevation="0" >
-            <v-icon>mdi-arrow-u-left-bottom</v-icon>
-          </v-btn>
-        </a>
-        </div>
-      </div>
-      <div v-else>
+              <h3 class="d-none">Preferencias alimenticias: </h3>
 
-        <h3>Nombre: </h3>
+              <p class="d-none">N/A</p>
 
-        <p>{{ this.datosApp.invitado.nombreInvitado }}</p>
+              <h3 class="d-none">Alergias alimenticias: </h3>
 
-        <h3>Número de acompañantes: </h3>
+              <p class="d-none">N/A</p>
 
-        <p>{{ this.datosApp.invitado.totalAsistentes }}</p>
+              <p class="d-none">
+                Puedes cambiar tu selección antes del:
+              </p>
 
-        <h3>Preferencias alimenticias: </h3>
+              <p class="pt-4 subtext d-none"> {{ deadline }} a las 23:00 hrs</p>
 
-        <p>N/A</p>
+              <span>Gracias, te esperamos</span><br>
 
-        <h3>Alergias alimenticias: </h3>
-
-        <p>N/A</p>
-
-        <p>
-            Puedes cambiar tu selección antes del:
-        </p>
-
-        <p class="pt-4 subtext"> {{ deadline }} a las 23:00 hrs</p>
-
-        <span>Regresar a la página principal</span><br>
-
-        <a href="/">
-          <v-btn color="#E6C98A" class="white--text my-5" tile elevation="0" >
-            <v-icon>mdi-arrow-u-left-bottom</v-icon>
-          </v-btn>
-        </a>
+              <a :href="urlActual">
+                <v-btn color="#E6C98A" class="white--text my-5" tile elevation="0">
+                  Finalizar
+                </v-btn>
+              </a>
 
             </div>
           </div>
@@ -100,42 +95,44 @@
 import { mapActions, mapState } from 'vuex';
 
 import * as moment from 'moment';
-  
-    export default {
-      name: 'Seccion3',
-  
-      components: {
-      },
-  
-      data: () => ({
-        totalAsistentes: "0",
-        visibleAsistentes: true
-      }),
 
-      computed: {
-        ... mapState(['visibles', 'datosApp']),
-        deadline() {
-          moment.locale('es-mx');
-          return moment(this.datosApp.evento.fechaEvento).subtract(2, 'months').format('LL');
-        },
-        arrayAsistentes() {
-          let arrayRespuesta = [];
-          for( let i = 1; i <= this.datosApp.invitado.totalAsistentes; i++  ) {
-            arrayRespuesta.push(i);
-          }
-          return arrayRespuesta;
-        }
-      },
-  
-      methods: {
-        ... mapActions(['numeroAsistentes']),
+export default {
+  name: 'Seccion3',
 
-        setNumeroAsistentes() {
-          this.visibleAsistentes = false;
-          this.numeroAsistentes(this.totalAsistentes);
-        },
-        
+  components: {
+  },
+
+  data: () => ({
+    totalAsistentes: "0",
+    visibleAsistentes: true
+  }),
+
+  computed: {
+    ...mapState(['visibles', 'datosApp']),
+    deadline() {
+      moment.locale('es-mx');
+      return moment(this.datosApp.evento.fechaEvento).subtract(2, 'months').format('LL');
+    },
+    arrayAsistentes() {
+      let arrayRespuesta = [];
+      for (let i = 1; i <= this.datosApp.invitado.totalAsistentes; i++) {
+        arrayRespuesta.push(i);
       }
+      return arrayRespuesta;
+    },
+    urlActual() {
+      return window.location.href;
     }
-  </script>
-  
+  },
+
+  methods: {
+    ...mapActions(['numeroAsistentes']),
+
+    setNumeroAsistentes() {
+      this.visibleAsistentes = false;
+      this.numeroAsistentes(this.totalAsistentes);
+    },
+
+  }
+}
+</script>
